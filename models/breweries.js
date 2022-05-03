f0758dcf8302b92381875eaeb018252ed57321c7
ruben <rubendasilva@me.com>
2022-05-03 10:50:54 -0700

    adding models

---
+++ models/breweries.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 5
+    },
+    user: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+});
+
+const brewerySchema = new Schema({
+    
+})
