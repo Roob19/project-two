329b67eb288bc1932c97a37cfb1d119a7cf17c3b
ruben <rubendasilva@me.com>
2022-05-02 20:20:53 -0700

    end of night

--- conifg/database.js
+++ conifg/database.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const db = mongoose.connection;
 
-mogoose.connect('process.env.DATABASE_URL', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mogoose.connect("process.env.DATABASE_URL", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-db.on('connected', function () {
-    console.log(`connected to mongoDB ${db.name} at ${db.host} : ${db.port}`);
+db.on("connected", function () {
+  console.log(`connected to mongoDB ${db.name} at ${db.host} : ${db.port}`);
 });
