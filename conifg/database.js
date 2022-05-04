fe1a23a9e5b15dde993b03d8b7932fed9f6f144a
ruben <rubendasilva@me.com>
2022-05-04 13:29:25 -0700

    reinstalled mongo and new cluster

--- conifg/database.js
+++ conifg/database.js
@@ -1,12 +1,9 @@
-const mongoose = require("mongoose");
-const db = mongoose.connection;
 
-mongoose.connect("process.env.DATABASE_URL", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-db.on("connected", function () {
-  console.log(`connected to mongoDB ${db.name} at ${db.host} : ${db.port}`);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DATABASE_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
