3cfca8b92bc24dd565c42f4dc5bca9801370526b
ruben <rubendasilva@me.com>
2022-05-03 20:51:43 -0700

    end of night

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const passport = require("passport");
+const fetch = require('node-fetch');
+const rootURL = 'https://api.openbrewerydb.org/breweries/autocomplete?query=';
+const byCityURL = 'https://api.openbrewerydb.org/breweries?by_city=';
+const byPostalURL = 'https://api.openbrewerydb.org/breweries?by_postal=';
+const byLatLonURL = 'https://api.openbrewerydb.org/breweries?by_dist=';
+const byStateURL = 'https://api.openbrewerydb.org/breweries?by_state=';
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -25,6 +32,16 @@ router.get(
 router.get("/logout", function (req, res) {
   req.logout();
   res.redirect("/pageView/home");
+});
+
+router.get('/', function( req, res, next) {
+  const postal_code = req.query.postal_code;
+  if ( !postal_code ) return res.render('index', {breweryData: null});
+  fetch(`${byPostalURL}${postal_code}`)
+  .then(res => res.json())
+  .then(breweryData => {
+    res.render('index', {title: breweryData.name, breweryData});
+  });
 });
 
 module.exports = router;
