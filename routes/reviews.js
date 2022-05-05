0b5993045f11025f0d119d378d091974715d0f4a
ruben <rubendasilva@me.com>
2022-05-05 10:38:11 -0700

    changed /routes/index.js line 18 .get to .post

--- routes/reviews.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reviewsCtrl = require("../controllers/reviews");
-
-router.post("/pageView/:id/reviews", reviewsCtrl.create);
-router.put("/pageView/:id/reviews", reviewsCtrl.update);
-router.delete("/reviews/:id", reviewsCtrl.delete);
-
-module.exports = router;
--- seeds.js
+++
@@ -1,6 +0,0 @@
-require('./config/database');
-const Brewery = require('./models/brewery');
-const User = require('./models/user');
-const data = require('./data');
-
-// const p1 = Brewery.deleteMany({});
