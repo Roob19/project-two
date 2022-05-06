49c6420962fcb40d211bf447c097d994ba5bb1d5
ruben <rubendasilva@me.com>
2022-05-05 17:58:58 -0700

    edit

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get(
 router.get(
   "/oauth2callback",
   passport.authenticate("google", {
-    successRedirect: "/pageView/index",
+    successRedirect: "/breweries",
     failureRedirect: "/pageView",
   })
 );
