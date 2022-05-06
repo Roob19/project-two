4953cf9f3855eb1710fc08eaa27addc6f33ecc08
ruben <rubendasilva@me.com>
2022-05-05 18:45:13 -0700

    edit

--- pu.sh
+++ pu.sh
@@ -1,8 +1,12 @@
-add .
-echo
-git commit -m "minor tweek"
-echo
-git push -u origin main
-echo
+add . &
+echo "Done."
+wait
+
+git commit -m "minor tweek" &
+echo "Done."
+wait
+git push -u origin main &
+echo "Done."
+wait
 git push heroku main
 echo '<><><><><>'
