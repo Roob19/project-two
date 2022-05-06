c30fbe3474e4829cdff79e051bc321784f41c91c
ruben <rubendasilva@me.com>
2022-05-05 23:23:12 -0700

    typo

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const reviewsCtrl = require("../controllers/reviews");
 
 router.post("/breweries/:id/reviews", reviewsCtrl.create);
-// router.get("/reviews/:id/edit", reviewsCtl.edit);
+router.get("/reviews/:id/edit", reviewsCtrl.edit);
 router.put("/reviews/:id", reviewsCtrl.update);
 router.delete("/reviews/:id", reviewsCtrl.delete);
 
